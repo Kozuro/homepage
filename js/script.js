ba1b00d4b391da55682a896845c6e2a3e54e77a2
Wojciech <duttybwooy@gmail.com>
2022-12-21 09:01:47 +0100

    renamed class light on lightTheme

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 {
-const welcome = () => {
-    console.log("Hello!");
-}
+    const welcome = () => {
+        console.log("Hello!");
+    }
 
-const toggleBackground = () => {
-    const body = document.querySelector(".js-body");
-    const themeName = document.querySelector(".js-themeName");
+    const toggleBackground = () => {
+        const body = document.querySelector(".js-body");
+        const themeName = document.querySelector(".js-themeName");
 
-    body.classList.toggle("light");
-    themeName.innerText = body.classList.contains("light") ? "Ciemny" : "Jasny";
-}
+        body.classList.toggle("lightTheme");
+        themeName.innerText = body.classList.contains("lightTheme") ? "Ciemny" : "Jasny";
+    }
 
-const init = () => {
-    const changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
-    changeBackgroundButton.addEventListener("click", toggleBackground);
+    const init = () => {
+        const changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
+        changeBackgroundButton.addEventListener("click", toggleBackground);
 
-    welcome();
-}
+        welcome();
+    }
 
-init();
+    init();
 }
